b00f1d26df0ab17b8634230f44912a1bf93fc365
mattymatt1994 <jedimatt.jordan@gmail.com>
2023-03-13 16:39:56 -0400

    Rework exercise 3

--- app.js
+++ app.js
@@ -4,16 +4,71 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+var a = 20;
+var b = 4;
+var add = a + b;
+var minus = a - b;
+var multiply = a * b;
+var divide = a / b;
 
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
 
+
+//  "1111"
+// "11eleven"
+// 12
+//"Frodo11"
+//"True11"
+//"FrodoBaggins"
+
+let num = 11
+let str = "11"
+let str2 = "eleven"
+let isPresent = true
+let firstName = "Frodo"
+let lastName = "Baggins"
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName +" " + lastName);
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+console.log(val == str3);//true
+console.log(val === str3);//false
+console.log(!isPresent2)//true
+console.log("eleven" == str4 && val >= str3);//false
+console.log(!isPresent2||isPresent2);//true
+console.log();
+console.log(true);
+console.log(NaN === false);
+console.log(false);
+console.log(true);
+console.log(false);
+/*True
+  *False
+  *True
+  *False
+  *True
+  *True
+  *True
+  *NaN === false
+  *false
+  *true
+  */
